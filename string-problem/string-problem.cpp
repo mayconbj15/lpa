12b4f16ea06decb6f6c721fbab65ab874e5e78f6
mayconbj15 <mayconbj15@gmail.com>
2020-02-24 12:06:51 -0300

    terminando solução problema das strings

--- string-problem/string-problem.cpp
+++ string-problem/string-problem.cpp
@@ -1,55 +1,145 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
 string tirarEspaco(string palavra);
+string tirarEspacosInicio(string palavra);
 
-void mostrarPalavras(string palavras[], int n){
-    for(int i=0; i < n; i++){
+void mostrarPalavras(string palavras[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
         cout << palavras[i] << endl;
     }
 }
 
-void tirarEspacos(string palavras[], int n){
-    for(int i=0; i < n; i++){
-        tirarEspaco(palavras[i]);
+string *tirarEspacos(string palavras[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        palavras[i] = tirarEspacosInicio(palavras[i]);
+        palavras[i] = tirarEspaco(palavras[i]);
     }
+
+    return palavras;
 }
 
-string tirarEspaco(string palavra){
+string tirarEspacosInicio(string palavra)
+{
+    string stringAux = "";
     int i = 0;
-    
-    string stringAux("");
-
-    while(i < palavra.length()){
-        if(palavra[i] != ' '){
-            stringAux[i] += palavra[i];
+    if (palavra[0] == ' ')
+    {
+        while (palavra[i] == ' ')
+        {
+            i++;
         }
-            
+
+        stringAux = palavra.substr(i, palavra.length());
+    }
+    else
+    {
+        return palavra;
+    }
+    return stringAux;
+}
+
+string tirarEspaco(string palavra)
+{
+    int i = 0;
+
+    string stringAux = "";
+
+    while (i < palavra.length())
+    {
+        if (palavra[i] != ' ')
+            stringAux += palavra[i];
+        else if (i + 1 < palavra.length() && palavra[i] == ' ' && palavra[i + 1] != ' ')
+            stringAux += palavra[i];
 
         i++;
     }
 
-    cout << palavra << endl;
-    cout << stringAux << endl;
+    return stringAux;
 }
 
-int main(){
-    int n;
-    
-    cin >> n;
+int tamanhoMaiorPalavra(string palavras[], int n)
+{
+    int maiorTamanho = 0;
 
-    fflush(stdin); //limpa buffer de entrada
-    
-    string palavras[n];
-
-    for(int i=0; i < n; i++){
-        getline(cin, palavras[i]);
+    for (int i = 0; i < n; i++)
+    {
+        if (palavras[i].length() > maiorTamanho)
+            maiorTamanho = palavras[i].length();
     }
 
-    mostrarPalavras(palavras, n);
+    return maiorTamanho;
+}
 
-    tirarEspacos(palavras, n);
-    
+void mostraCentralizadoDireita(string palavras[], int n, int maiorTamanho)
+{
+    for (int i = 0; i < n; i++)
+    {
+        int x = maiorTamanho - palavras[i].length();
+
+        for (int j = 0; j < palavras[i].length(); x--)
+        {
+            if (x > 0)
+                cout << ' ';
+            else
+            {
+                cout << palavras[i][j];
+                j++;
+            }
+        }
+        cout << endl;
+    }
+}
+
+bool stringVazia(string palavra)
+{
+    bool vazia = true;
+
+    for (int i = 0; i < palavra.length(); i++)
+    {
+        if (palavra.at(i) != ' ')
+            vazia = false;
+    }
+
+    return vazia;
+}
+
+int main()
+{
+    int n;
+
+    cin >> n;
+
+    do
+    {
+        fflush(stdin); //limpa buffer de entrada
+
+        string palavras[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            getline(cin, palavras[i]);
+
+            if (stringVazia(palavras[i]))
+                i--;
+        }
+
+        // mostrarPalavras(palavras, n);
+
+        tirarEspacos(palavras, n);
+
+        //mostrarPalavras(palavras, n);
+
+        int maiorTamaho = tamanhoMaiorPalavra(palavras, n);
+
+        mostraCentralizadoDireita(palavras, n, maiorTamaho);
+
+        cin >> n;
+    } while (n != 0);
 }
